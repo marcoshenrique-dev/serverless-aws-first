51a4832e219bd2ea359fb436c103df9ba6a516a6
Marcos Henrique <marcoshenriqueh393@gmail.com>
2020-10-20 17:07:41 -0300

    feat: create listen dogs and production config

---
+++ __mocks__/dogs.js
@@ -0,0 +1,25 @@
+module.exports.dogsList = [
+
+   {
+      "name": "Scooby",
+      "age": 4,
+      "race": "dachshund"
+   },
+
+   {
+      "name": "Ben",
+      "age": 10,
+      "race": "bull dogie"
+   },
+   {
+      "name": "Trovão",
+      "age": 7,
+      "race": "border collie"
+   },
+   {
+      "name": "serverless",
+      "age": 5,
+      "race": "not defined"
+   },
+
+];
--- src/application/handlers/handler.js
+++ src/application/handlers/handler.js
@@ -1,12 +1,13 @@
 'use strict';
 
-module.exports.hello = async event => {
+const dogsList = require("../../../__mocks__/dogs");
+
+module.exports.dogs = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        dogsList
       },
       null,
       2
